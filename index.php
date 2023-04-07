a046fd35b9bf85c3e837d665709aced96635e15f
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-08 01:25:08 +0200

    Add CSS

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Register - Out Website</title>
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/form.css">
 </head>
 <body>
   <?php include "header.php" ?>
