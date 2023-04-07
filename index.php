35f7efdbad2033e3d562fb6993c94d36c32cbde3
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-07 23:14:50 +0200

    Initial page setup

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register - Out Website</title>
+</head>
+<body>
+  <?php include "header.php" ?>
+  <div class="form">
+    <h2>Register</h2>
+    <div class="container">
+      <div class="fields">
+
+      </div>
+      <div class="side">
+        <div class="image"></div>
+        <div class="actors">
+          <h6>Actors born on the same day:</h6>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include "footer.php" ?>
+</body>
+</html>
