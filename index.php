3ff254c6417fbcfc3dceb0ea35436cb7d025eb98
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-08 15:21:28 +0200

    wrap everything in form element

--- index.php
+++ index.php
@@ -14,32 +14,34 @@
 <body>
   <?php include "header.php" ?>
   <div class="form">
-    <h2>Register</h2>
-    <div class="container">
-      <div class="fields">
-        <input type="text" name="full_name" placeholder="Full name">
-        <input type="text" name="username" placeholder="Username">
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
-        <input type="password" name="confirm_password" placeholder="Confirm password">
-        <input type="text" name="birthdate" placeholder="Birthdate" onfocus="(this.type='date')" onblur="(this.type='text')">
-        <input type="tel" name="phone" placeholder="Phone">
-        <input type="text" name="address" placeholder="Address">
-      </div>
-      <div class="side">
-        <div class="image">
-          <input type="file" name="user_image" id="file-upload">
-          <img src="" height="200" alt="User image preview">
-          <div class="no-image">
-            <div class="icon upload-icon"></div>
-            <p>Upload an image</p>
-          </div>
+    <form action="" method="post">
+      <h2>Register</h2>
+      <div class="container">
+        <div class="fields">
+          <input type="text" name="full_name" placeholder="Full name">
+          <input type="text" name="username" placeholder="Username">
+          <input type="email" name="email" placeholder="Email">
+          <input type="password" name="password" placeholder="Password">
+          <input type="password" name="confirm_password" placeholder="Confirm password">
+          <input type="text" name="birthdate" placeholder="Birthdate" onfocus="(this.type='date')" onblur="(this.type='text')">
+          <input type="tel" name="phone" placeholder="Phone">
+          <input type="text" name="address" placeholder="Address">
         </div>
-        <h3>Actors born on the same day:</h3>
-        <div class="actors"></div>
-        <button>Register</button>
+        <div class="side">
+          <div class="image">
+            <input type="file" name="user_image" id="file-upload">
+            <img src="" height="200" alt="User image preview">
+            <div class="no-image">
+              <div class="icon upload-icon"></div>
+              <p>Upload an image</p>
+            </div>
+          </div>
+          <h3>Actors born on the same day:</h3>
+          <div class="actors"></div>
+          <button type="submit">Register</button>
+        </div>
       </div>
-    </div>
+    </form>
   </div>
   <?php include "footer.php" ?>
 </body>
