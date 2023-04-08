2f8dff024ae485268b4fd76aecac12e73ad1d425
mo <a>
2023-04-08 16:15:00 +0200

    Edit Header and footer css

--- header.php
+++ header.php
@@ -1,6 +1,12 @@
 <div class="header">
     <div class="title"><h3>Our website</h3>
     <div class="my-icon"></div>
+    <div class="theme-switch-container">
+                <label class="theme-slider" for="checkbox">
+                    <input type="checkbox" id="checkbox" />
+                    <div class="round slider"></div>
+                </label>
+            </div>
 </div>
     
 </div>
