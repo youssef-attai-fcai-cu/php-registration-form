c9041ce978dc9c53203036cce7be5667ac864d3f
Amr <lewolfje@gmail.com>
2023-04-10 01:17:57 +0200

    fixed input validations & appropriate error highlights

--- js/validations.js
+++ js/validations.js
@@ -33,6 +33,33 @@ const submitButton = document.querySelector(`button[type="submit"]`);
 const imageDiv = document.querySelector(".image");
 const uploadImgInput = document.querySelector('input[type="file"]');
 
+// function checkEmptyFields() {
+//   let missing = 0;
+
+//   // foreach loop to check if the input fields are empty
+//   allInputs.forEach((input) => {
+//     if (input.value === ``) {
+//       input.classList.add("invalid");
+//       input.setAttribute("title", "This field is required");
+//       missing++;
+//     } else {
+//       input.classList.remove("invalid");
+//       input.removeAttribute("title");
+//     }
+//   });
+
+//   if (uploadImgInput.value === ``) {
+//     imageDiv.classList.add("invalid");
+//     imageDiv.setAttribute("title", "You have to upload an image");
+//     missing++;
+//   } else {
+//     imageDiv.classList.remove("invalid");
+//     imageDiv.removeAttribute("title");
+//   }
+
+//   return missing == 0;
+// }
+
 function checkEmptyFields() {
   let missing = 0;
 
@@ -45,6 +72,77 @@ function checkEmptyFields() {
     } else {
       input.classList.remove("invalid");
       input.removeAttribute("title");
+      switch (input.name) {
+        case "full_name":
+          if (!validateFullName()) {
+            input.classList.add("invalid");
+            input.setAttribute(
+              "title",
+              "Please enter a valid full name (letters and spaces only)"
+            );
+            missing++;
+          }
+          break;
+        case "username":
+          if (!validateUsername()) {
+            input.classList.add("invalid");
+            input.setAttribute(
+              "title",
+              "Please enter a valid username (letters and digits only)"
+            );
+            missing++;
+          }
+          break;
+        case "email":
+          if (!validateEmail()) {
+            input.classList.add("invalid");
+            input.setAttribute("title", "Please enter a valid email address");
+            missing++;
+          }
+          break;
+        case "password":
+          if (!validatePassword()) {
+            input.classList.add("invalid");
+            input.setAttribute(
+              "title",
+              "Please enter a valid password (minimum 8 characters, at least one digit, at least one special character"
+            );
+            missing++;
+          }
+          break;
+        case "confirm_password":
+          if (!validateConfirmPassword()) {
+            input.classList.add("invalid");
+            input.setAttribute("title", "Passwords do not match");
+            missing++;
+          }
+          break;
+        case "birthdate":
+          // do nothing for birthdate, as we don't need to validate it
+          break;
+        case "phone":
+          if (!validatePhoneNumber()) {
+            input.classList.add("invalid");
+            input.setAttribute(
+              "title",
+              "Please enter a valid phone number (11 digits only)"
+            );
+            missing++;
+          }
+          break;
+        case "address":
+          if (!validateAddress()) {
+            input.classList.add("invalid");
+            input.setAttribute(
+              "title",
+              "Please enter a valid address (letters, digits, spaces, commas, apostrophes, and hyphens only)"
+            );
+            missing++;
+          }
+          break;
+        default:
+          break;
+      }
     }
   });
 
@@ -60,14 +158,14 @@ function checkEmptyFields() {
   return missing == 0;
 }
 
-// Validate the Full Name Field, Allowed: constters and spaces only
+// Validate the Full Name Field, Allowed: letters and spaces only
 function validateFullName() {
   const fullNameRegex = /^[a-zA-Z\s]+$/;
   if (!fullNameRegex.test(fullNameInput.value)) {
     fullNameInput.classList.add("invalid");
     fullNameInput.setAttribute(
       "title",
-      "Please enter a valid full name (constters and spaces only)"
+      "Please enter a valid full name (letters and spaces only)"
     );
     return false;
   } else {
@@ -76,7 +174,7 @@ function validateFullName() {
     return true;
   }
 }
-// Validate the Email Field, Allowed: email format only which uses constters, numbers, @, and .
+// Validate the Email Field, Allowed: email format only which uses letters, numbers, @, and .
 function validateEmail() {
   const emailRegex = /^\S+@\S+\.\S+$/;
   if (!emailRegex.test(emailInput.value)) {
@@ -90,32 +188,11 @@ function validateEmail() {
   }
 }
 
-// Validate the Password Field, Allowed: minimum 8 Characters, At least one digit, atleast one special character
+// Validate the Password Field, Allowed: minimum 8 characters, at least one digit, at least one special character
 function validatePassword() {
+  const passwordRegex = /^(?=.*\d)(?=.*[\W_]).{8,}$/;
   const pwd = passwordInput.value;
-
-  let hasDigit = false;
-  const specialChars = `!@#$%^&*()_+~\`|}{[]\:;?><,./-="`;
-  let hasSpecialChar = false;
-  let isVaild = true;
-
-  if (pwd.length < 8) {
-    isVaild = false;
-  }
-
-  for (let i = 0; i < pwd.length; i++) {
-    if (!isNaN(pwd[i])) {
-      hasDigit = true;
-    } else if (specialChars.includes(pwd[i])) {
-      hasSpecialChar = true;
-    }
-  }
-
-  if (!hasDigit || !hasSpecialChar) {
-    isVaild = false;
-  }
-
-  if (!isVaild) {
+  if (!passwordRegex.test(pwd)) {
     passwordInput.classList.add("invalid");
     passwordInput.setAttribute(
       "title",
@@ -151,21 +228,22 @@ function validatePhoneNumber() {
       "title",
       "Please enter a valid phone number (11 digits only)"
     );
-    return `Please enter a valid phone number`;
+    return false;
   } else {
     phoneInput.classList.remove("invalid");
     phoneInput.removeAttribute("title");
+    return true;
   }
 }
 
-// Validate the Username Field, Allowed: constters and digits only
+// Validate the Username Field, Allowed: letters and digits only
 function validateUsername() {
   const usernameRegex = /^[a-zA-Z0-9]+$/;
   if (!usernameRegex.test(usernameInput.value)) {
     usernameInput.classList.add("invalid");
     usernameInput.setAttribute(
       "title",
-      "Please enter a valid username (constters and digits only)"
+      "Please enter a valid username (letters and digits only)"
     );
     return false;
   } else {
@@ -175,7 +253,7 @@ function validateUsername() {
   }
 }
 
-// Validate the Address Field, Allowed: constters, digits, spaces, commas, apostrophes, and hyphens only
+// Validate the Address Field, Allowed: letters, digits, spaces, commas, apostrophes, and hyphens only
 function validateAddress() {
   const addressRegex = /^[a-zA-Z0-9\s,'-]*$/;
   if (!addressRegex.test(addressInput.value)) {
@@ -195,15 +273,5 @@ function validateAddress() {
 // Grouping all the validations together and calling them when the register button is clicked
 // If there are any errors, the user will be alerted with the respective error message
 submitButton.addEventListener(`click`, (e) => {
-  // if there are any no empty fields
-  if (checkEmptyFields()) {
-    // check validity of each field
-    validateFullName();
-    validateEmail();
-    validatePassword();
-    validateConfirmPassword();
-    validatePhoneNumber();
-    validateUsername();
-    validateAddress();
-  }
+  checkEmptyFields();
 });
