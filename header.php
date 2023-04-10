95001638e54dec8dd269e8ed8f74c4ff885991f1
Sherif <sherif3hassan@gmail.com>
2023-04-10 22:01:16 +0200

    Edit header and footer

--- header.php
+++ header.php
@@ -1,12 +1,14 @@
 <div class="header">
     <div class="title"><h3>Our website</h3>
-    <div class="my-icon"></div>
+    <div class="container-header">
     <div class="theme-switch-container">
                 <label class="theme-slider" for="checkbox">
                     <input type="checkbox" id="checkbox" />
                     <div class="round slider"></div>
                 </label>
             </div>
+            <a href="#"class="my-icon" target="_blank"></a>
+    </div>
 </div>
     
 </div>
