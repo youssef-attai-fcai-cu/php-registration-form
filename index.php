af80e546a32d0d4a0d213f283096967fafa6a124
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-10 21:20:54 +0200

    Check if username already exists

--- index.php
+++ index.php
@@ -89,7 +89,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $address = $_POST['address'];
 
   // Make sure the username is unique
-  
+  if ($database->user_exists($username)) {
+    die('Username already exists');
+  }
 
   die('Success');
 }
@@ -119,34 +121,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
   <?php include "header.php" ?>
   <div class="wrapper">
-  <div class="form">
-    <h2>Register</h2>
-    <div class="container">
-      <div class="fields">
-        <input type="text" name="full_name" placeholder="Full name">
-        <input type="text" name="username" placeholder="Username">
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
-        <input type="password" name="confirm_password" placeholder="Confirm password">
-        <input type="text" name="birthdate" placeholder="Birthdate" onfocus="(this.type='date')" onblur="(this.type='text')">
-        <input type="tel" name="phone" placeholder="Phone">
-        <input type="text" name="address" placeholder="Address">
-      </div>
-      <div class="side">
-        <div class="image">
-          <input type="file" name="user_image">
-          <img src="" height="200" alt="User image preview">
-          <div class="no-image">
-            <div class="icon upload-icon"></div>
-            <p>Upload an image</p>
-          </div>
+    <div class="form">
+      <h2>Register</h2>
+      <div class="container">
+        <div class="fields">
+          <input type="text" name="full_name" placeholder="Full name">
+          <input type="text" name="username" placeholder="Username">
+          <input type="email" name="email" placeholder="Email">
+          <input type="password" name="password" placeholder="Password">
+          <input type="password" name="confirm_password" placeholder="Confirm password">
+          <input type="text" name="birthdate" placeholder="Birthdate" onfocus="(this.type='date')" onblur="(this.type='text')">
+          <input type="tel" name="phone" placeholder="Phone">
+          <input type="text" name="address" placeholder="Address">
         </div>
-        <h3>Actors born on the same day:</h3>
-        <div class="actors"></div>
-        <button type="submit">Register</button>
+        <div class="side">
+          <div class="image">
+            <input type="file" name="user_image">
+            <img src="" height="200" alt="User image preview">
+            <div class="no-image">
+              <div class="icon upload-icon"></div>
+              <p>Upload an image</p>
+            </div>
+          </div>
+          <h3>Actors born on the same day:</h3>
+          <div class="actors"></div>
+          <button type="submit">Register</button>
+        </div>
       </div>
     </div>
-  </div>
   </div>
   <?php include "footer.php" ?>
 </body>
