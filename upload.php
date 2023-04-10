749ad8196ade5dcb4fa0291494a454f900933998
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-10 22:12:35 +0200

    Wrap file upload in a function

--- upload.php
+++ upload.php
@@ -1,45 +1,47 @@
 <?php
 
-// Directory to upload files to
-$target_dir = "uploads/";
+function uploadImage()
+{
+  // Directory to upload files to
+  $target_dir = "uploads/";
 
-// Get the file name and extension
-$target_file = $target_dir . basename($_FILES["user_image"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+  // Get the file name and extension
+  $target_file = $target_dir . basename($_FILES["user_image"]["name"]);
+  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-$uploadOk = 1;
+  $uploadOk = 1;
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  try {
-    $check = getimagesize($_FILES["user_image"]["tmp_name"]);
-    if($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
-      $uploadOk = 1;
-    } else {
-      echo "File is not an image.";
+  // Check if image file is a actual image or fake image
+  if (isset($_POST["submit"])) {
+    try {
+      $check = getimagesize($_FILES["user_image"]["tmp_name"]);
+      if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+      } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    } catch (ValueError $th) {
       $uploadOk = 0;
     }
-  } catch (ValueError $th) {
+  }
+
+  // Allow certain file formats
+  if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo "Sorry, only JPG, JPEG, PNG files are allowed.";
     $uploadOk = 0;
   }
-}
 
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-  echo "Sorry, only JPG, JPEG, PNG files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["user_image"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["user_image"]["name"])). " has been uploaded.";
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    if (move_uploaded_file($_FILES["user_image"]["tmp_name"], $target_file)) {
+      echo "The file " . htmlspecialchars(basename($_FILES["user_image"]["name"])) . " has been uploaded.";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
   }
 }
-?>
