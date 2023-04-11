13298aa4dc030bdf62839fc76bead85edb4af338
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-11 16:01:23 +0200

    Make header and footer match the designs on figma

--- header.php
+++ header.php
@@ -1,14 +1,12 @@
 <div class="header">
-    <div class="title"><h3>Our website</h3>
+    <h3>Our website</h3>
     <div class="container-header">
-    <div class="theme-switch-container">
-                <label class="theme-slider" for="checkbox">
-                    <input type="checkbox" id="checkbox" checked />
-                    <div class="round slider"></div>
-                </label>
-            </div>
-            <a href="#"class="my-icon" target="_blank"></a>
+        <div class="theme-switch-container">
+            <label class="theme-slider" for="checkbox">
+                <input type="checkbox" id="checkbox" checked />
+                <div class="round slider"></div>
+            </label>
+        </div>
+        <a href="#" class="my-icon" target="_blank"></a>
     </div>
-</div>
-    
 </div>
