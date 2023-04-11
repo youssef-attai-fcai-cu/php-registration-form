5d6332c660cb7bbf0cea5c33875e75beec784357
youssef-attai <youssefgalalnazem@gmail.com>
2023-04-11 16:06:36 +0200

    Remove redundant container

--- header.php
+++ header.php
@@ -1,16 +1,12 @@
 <div class="header">
-    <div class="title">
-        <h3>Our website</h3>
-        <div class="container-header">
-            <div class="theme-switch-container">
-                <input type="checkbox" id="toggle">
-                <label class="toggle" for="toggle">
-                    <i class="icon bx-moon"></i>
-                    <i class="icon bx-sun"></i>
-                    <span class="ball"></span>
-                </label>
-            </div>
-            <a href="#" class="my-icon" target="_blank"></a>
-        </div>
+    <h3>Our website</h3>
+    <div class="container-header">
+        <input type="checkbox" id="toggle">
+        <label class="toggle" for="toggle">
+            <i class="icon bx-moon"></i>
+            <i class="icon bx-sun"></i>
+            <span class="ball"></span>
+        </label>
+        <a href="#" class="my-icon" target="_blank"></a>
     </div>
 </div>
