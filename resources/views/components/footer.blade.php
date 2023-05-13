cb2e8dd13a9e6d67f72db5bc49e7a443ac492c91
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-05-13 21:39:48 +0300

    Add github profiles instead of linkedin in credits

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,16 +3,16 @@
     <h2><?php echo __('strings.madeby') ?>:</h2>
         <div class="madeby">
             <div>
-                <h4>Sherif Hassan</h4><a href="https://www.linkedin.com/in/sherif3hassan/">Sherif Hassan</a>
+                <h4>Sherif Hassan</h4><a href="https://www.github.com/sherif3hassan/">@sherif3hassan</a>
             </div>
             <div>
-                <h4>Amr Ibrahim</h4><a href="https://www.linkedin.com/in/amr-ibrahim-4947b2243/">Amr Ibrahim</a>
+                <h4>Amr Ibrahim</h4><a href="https://www.github.com/v1AIM/">@v1AIM</a>
             </div>
             <div>
-                <h4>Amr Khaled</h4><a href="https://www.linkedin.com/in/amr-k-ahmed/">Amr Khaled</a>
+                <h4>Amr Khaled</h4><a href="https://www.github.com/Le-Wolfie/">@Le-Wolfie</a>
             </div>
             <div>
-                <h4>Youssef Galal</h4><a href="https://www.linkedin.com/in/youssef-attai/">Youssef Galal</a>
+                <h4>Youssef Galal</h4><a href="https://www.github.com/youssef-attai/">@youssef-attai</a>
             </div>
         </div>
     </div>
